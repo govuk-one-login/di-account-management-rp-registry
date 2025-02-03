916edd9103ddaa921693cd7613971604943229e4
Saral Kaushik <saral.kaushik@digital.cabinet-office.gov.uk>
2025-02-03 12:18:59 +0000

    add initial client interface

---
+++ interfaces/client.interface.ts
@@ -0,0 +1,42 @@
+type EnvironmentValue<T> = T | { 
+    production: T, 
+    integration?: T,
+    nonProduction: T
+}
+
+type Translations = {
+    header: string,
+    description: string,
+    linkText: string,
+    linkUrl: EnvironmentValue<string>
+}
+
+interface BaseClient {
+    clientId: EnvironmentValue<string>,
+    translations: {
+        en: Translations,
+    } 
+    isAllowed: boolean,
+    isHmrc: boolean,
+    isReportSuspiciousActivityEnabled: boolean,
+    showInClientSearch: EnvironmentValue<boolean>
+    isAvailableInWelsh: boolean,
+}
+
+interface WelshClient extends BaseClient {
+    isAvailableInWelsh: true,
+    translations: {
+        en: Translations,
+        cy: Translations
+    }
+}
+
+interface NonWelshClient extends BaseClient {
+    isAvailableInWelsh: false,
+    translations: {
+        en: Translations,
+        cy: never
+    }
+}
+
+export type Client = NonWelshClient | WelshClient;

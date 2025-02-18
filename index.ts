d8a2064a8f898c6fbbfccd67f5e6164ee5fa1ce3
Peter Fajemisin <peter.fajemisin@digital.cabinet-office.gov.uk>
2025-02-18 16:11:21 +0000

    OLH-2437 - Ensure we use the latest version of the library whenever we deploy

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
 import getTranslations from './src/get-translations'
+import {Translation} from './interfaces/translations.interface'
+import {TranslationsObject} from './interfaces/translations.interface'
 
 export { getTranslations }
+export type { Translation, TranslationsObject }

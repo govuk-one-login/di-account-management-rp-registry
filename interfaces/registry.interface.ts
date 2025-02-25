acd4d16b374329e5bf52e41a1a49ab40146d1555
Saral Kaushik <saral.kaushik@digital.cabinet-office.gov.uk>
2025-02-25 13:25:31 +0000

    create getClient function

---
+++ interfaces/registry.interface.ts
@@ -0,0 +1,9 @@
+export interface RegistryEntry {
+    clientId: string;
+    isAllowed: boolean;
+    clientType: "service" | "account";
+    isHmrc: boolean;
+    isReportSuspiciousActivityEnabled: boolean;
+    showInClientSearch: boolean;
+    isAvailableInWelsh: boolean;
+}

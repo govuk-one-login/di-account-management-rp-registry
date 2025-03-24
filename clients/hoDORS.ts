be4bf16b1a3ae1f1c507162920f73b78f2ff898b
Latif <latif.muhammad@digital.cabinet-office.gov.uk>
2025-03-24 10:18:19 +0000

    Onboarded Home Office DORS (#36)

---
+++ clients/hoDORS.ts
@@ -0,0 +1,26 @@
+import { Client } from "../interfaces/client.interface";
+
+const hoDORS: Client = {
+  clientId: {
+    production: "Y12SlXI_zqYj_z6FFtVHExHjEO8",
+    integration: "Y12SlXI_zqYj_z6FFtVHExHjEO8",
+    nonProduction: "hoDORS",
+  },
+  isAvailableInWelsh: false,
+  isAllowed: true,
+  clientType: "account",
+  isHmrc: false,
+  isReportSuspiciousActivityEnabled: false,
+  showInClientSearch: { production: false, nonProduction: false },
+  translations: {
+    en: {
+      header: "DBS online results service",
+      description:
+        "View your DBS result and share with employers or voluntary organisations.",
+      linkText: "Go to the DBS online results service",
+      linkUrl: "https://www.view-my-disclosure-result.dbsdigital.io/auth/login",
+    },
+  },
+};
+
+export default hoDORS;

fd0e8ec2955e1c837e6a9113e47ee334074fd095
Latif <latif.muhammad@digital.cabinet-office.gov.uk>
2025-04-03 10:26:29 +0100

    add Welsh translations for CO Client Service Jobs

--- clients/coClientServiceJobs.ts
+++ clients/coClientServiceJobs.ts
@@ -6,7 +6,7 @@ const coClientServiceJobs: Client = {
     integration: "JbFTEj93ueyJAvYy75HzIc1790I",
     nonProduction: "coClientServiceJobs",
   },
-  isAvailableInWelsh: false,
+  isAvailableInWelsh: true,
   isAllowed: true,
   clientType: "account",
   isHmrc: false,
@@ -19,6 +19,13 @@ const coClientServiceJobs: Client = {
       description:
         "Find jobs in the Civil Service and manage your applications.",
       linkText: "Go to Civil Service Jobs",
+      linkUrl: "http://beta.civilservicejobs.gov.uk/",
+    },
+    cy: {
+      header: "Swyddi'r Gwasanaeth Sifil",
+      description:
+        "Dod o hyd i swyddi yn y Gwasanaeth Sifil a rheoli eich ceisiadau.",
+      linkText: "Ewch i Swyddi'r Gwasanaeth Sifil",
       linkUrl: "http://beta.civilservicejobs.gov.uk/",
     },
   },

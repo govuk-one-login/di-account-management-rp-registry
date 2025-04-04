d6079f57654d3949a97431dd5662a1357147bea7
Latif <latif.muhammad@digital.cabinet-office.gov.uk>
2025-04-04 10:36:34 +0100

    [OLH-2528] Onboard RP CO Civil Service Jobs (#43)
    * Onboard icsDesnz
    
    * Onboard CO: Client Serivce Jobs
    
    * Update public service to show in client search
    
    * add Welsh translations for CO Client Service Jobs

---
+++ clients/coClientServiceJobs.ts
@@ -0,0 +1,34 @@
+import { Client } from "../interfaces/client.interface";
+
+const coClientServiceJobs: Client = {
+  clientId: {
+    production: "JbFTEj93ueyJAvYy75HzIc1790I",
+    integration: "JbFTEj93ueyJAvYy75HzIc1790I",
+    nonProduction: "coClientServiceJobs",
+  },
+  isAvailableInWelsh: true,
+  isAllowed: true,
+  clientType: "account",
+  isHmrc: false,
+  isReportSuspiciousActivityEnabled: false,
+  isActivityLogEnabled: false,
+  showInClientSearch: { production: false, nonProduction: false },
+  translations: {
+    en: {
+      header: "Civil Service Jobs",
+      description:
+        "Find jobs in the Civil Service and manage your applications.",
+      linkText: "Go to Civil Service Jobs",
+      linkUrl: "http://beta.civilservicejobs.gov.uk/",
+    },
+    cy: {
+      header: "Swyddi'r Gwasanaeth Sifil",
+      description:
+        "Dod o hyd i swyddi yn y Gwasanaeth Sifil a rheoli eich ceisiadau.",
+      linkText: "Ewch i Swyddi'r Gwasanaeth Sifil",
+      linkUrl: "http://beta.civilservicejobs.gov.uk/",
+    },
+  },
+};
+
+export default coClientServiceJobs;

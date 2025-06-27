5f53c6b58d93873016bbf1eaedcea4870c8002e6
Latif <latif.muhammad@digital.cabinet-office.gov.uk>
2025-06-27 11:44:24 +0100

    OLH-2737: add govukApp to registry

--- interfaces/registry.interface.ts
+++ interfaces/registry.interface.ts
@@ -1,7 +1,7 @@
 export interface RegistryEntry {
   clientId: string;
   isOffboarded: boolean;
-  clientType: "service" | "account" | "home" | "internal";
+  clientType: "service" | "account" | "home" | "internal" | "govukApp";
   isHmrc: boolean;
   isReportSuspiciousActivityEnabled: boolean;
   isActivityLogEnabled: boolean;

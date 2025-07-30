bcb2b8176c2d67f3879e08ec6d2c61ac421827e9
Latif <latif.muhammad@digital.cabinet-office.gov.uk>
2025-07-30 12:28:17 +0100

    OLH-2920: Fix flag, improve tests

--- clients/dbtSubmitCosmeticProductNotification.ts
+++ clients/dbtSubmitCosmeticProductNotification.ts
@@ -6,20 +6,20 @@ const dbtSubmitCosmeticProductNotification: Client = {
     integration: "JhyNhK1J8oe9-Mnn05HQbeKhttk",
     nonProduction: "dbtSubmitCosmeticProductNotification",
   },
-  isAvailableInWelsh: false,
+  isAvailableInWelsh: true,
   showInAccounts: true,
   showInServices: false,
   showDetailedCard: false,
   showInActivityHistory: true,
   showInDeleteAccount: true,
-  showInSearchableList: true,
+  showInSearchableList: false,
   translations: {
     en: {
       header: "Submit cosmetic product notifications",
       description:
         "Add details about your cosmetic product to the Submit cosmetic product notification service.",
       linkText: "Go to the Submit cosmetic product notification service",
-      linkUrl: "www.cosmetic-product-notifications.service.gov.uk",
+      linkUrl: "http://www.cosmetic-product-notifications.service.gov.uk",
       startText: "Submit cosmetic product notifications",
       startUrl: "http://www.cosmetic-product-notifications.service.gov.uk",
     },
@@ -28,7 +28,7 @@ const dbtSubmitCosmeticProductNotification: Client = {
       description:
         "Ychwanegwch fanylion am eich cynnyrch cosmetig i'r Gwasanaeth cyflwyno hysbysiad cynnyrch cosmetig.",
       linkText: "Ewch i'r Gwasanaeth cyflwyno hysbysiad cynnyrch cosmetig",
-      linkUrl: "www.cosmetic-product-notifications.service.gov.uk",
+      linkUrl: "http://www.cosmetic-product-notifications.service.gov.uk",
       startText: "Cyflwyno hysbysiad cynnyrch cosmetig",
       startUrl: "http://www.cosmetic-product-notifications.service.gov.uk",
     },

716388b3023f80f227bd5aa4f98634613bda5fc1
Dana Cotoran <dana.cotoran@digital.cabinet-office.gov.uk>
2025-08-28 12:03:10 +0100

    OLH-2884: Address linkUrl redundancy

--- clients/dwpFindYourPensions.ts
+++ clients/dwpFindYourPensions.ts
@@ -19,13 +19,7 @@ const dwpFindYourPensions: Client = {
       description:
         "Find and view all your pensions information.",
       linkText: "Go to Find your pensions",
-      linkUrl: {
-        nonProduction:
-          "https://find-your-pensions.service.gov.uk/",
-        integration: "https://find-your-pensions.service.gov.uk/",
-        production:
-          "https://find-your-pensions.service.gov.uk/",
-      },
+      linkUrl: "https://find-your-pensions.service.gov.uk/",
     },
   },
   isOffboarded: false,

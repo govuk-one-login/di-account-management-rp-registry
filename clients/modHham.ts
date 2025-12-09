91a555f4f0237d98acf106edcd6cd4044a7a7469
Jeremy Barrass <jeremy.barrass@digital.cabinet-office.gov.uk>
2025-12-09 17:48:26 +0000

    OLH-3546 Add client for MOD Home Hub Appointments Manager

---
+++ clients/modHham.ts
@@ -0,0 +1,29 @@
+import { Client } from "../interfaces/client.interface";
+
+const modHham: Client = {
+  clientId: {
+    production: "GQf70-tvgfHyQsdQhJ2-b80wJcs",
+    integration: "GQf70-tvgfHyQsdQhJ2-b80wJcs",
+    nonProduction: "modHham",
+  },
+  isAvailableInWelsh: false,
+  showInAccounts: false,
+  showInServices: true,
+  showDetailedCard: false,
+  showInActivityHistory: false,
+  showInDeleteAccount: false,
+  showInSearchableList: true,
+  translations: {
+    en: {
+      header: "A service run by the Ministry of Defence (MOD)",
+      linkText: "Go to your HomeHub appointments account.",
+      linkUrl: "https://homehub.pinnacleservicefamilies.co.uk/appts",
+      hintText: "Book a repair appointment.",
+      paragraph1:
+        "Manage appointments for repairs for your service family accommodation.",
+    },
+  },
+  isOffboarded: false,
+};
+
+export default modHham;

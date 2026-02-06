ae554ec8d0626738f9ab6696ec1c991e6626f2da
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2026-02-06 14:46:09 +0000

    OLH-3768: added Welsh content

--- clients/hmrc.ts
+++ clients/hmrc.ts
@@ -6,7 +6,7 @@ const hmrc: Client = {
     integration: "mQDXGO7gWdK7V28v82nVcEGuacY",
     nonProduction: "hmrc",
   },
-  isAvailableInWelsh: false,
+  isAvailableInWelsh: true,
   showInAccounts: true,
   showInServices: false,
   showDetailedCard: false,
@@ -20,6 +20,14 @@ const hmrc: Client = {
       linkUrl: "https://www.gov.uk/log-in-register-hmrc-online-services",
       description:
         "Search for HMRC services or sign in to find HMRC services you have added to your personal or business tax account.",
+    },
+    cy: {
+      header:
+        "Gwasanaethau sy'n cael eu rhedeg gan Cyllid a Thollau EF (CThEF)",
+      linkText: "Chwilio am neu fewngofnodi i ddod o hyd i wasanaethau CThEF",
+      linkUrl: "https://www.gov.uk/log-in-register-hmrc-online-services",
+      description:
+        "Chwiliwch am wasanaethau CThEF neu mewngofnodwch i ddod o hyd i wasanaethau CThEF rydych wedi'u hychwanegu at eich cyfrif treth bersonol neu fusnes.",
     },
   },
   isOffboarded: false,

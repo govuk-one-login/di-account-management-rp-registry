a1ed0a087cd00a1f52cd84feacbc7812e26aa857
Taher Kapasi <taher.kapasi@digital.cabinet-office.gov.uk>
2026-03-19 09:05:39 +0000

    OLH-3916: Victims Pathfinder to DAP and Heat Network Zoning Portal

---
+++ clients/mojVictimsPathFinderToDAP.ts
@@ -0,0 +1,27 @@
+import { Client } from "../interfaces/client.interface";
+
+const mojVictimsPathFinderToDAP: Client = {
+  clientId: {
+    production: "PhP3oy13CW0RwBTL7UUGgvSiaCg",
+    integration: "PhP3oy13CW0RwBTL7UUGgvSiaCg",
+    nonProduction: "mojVictimsPathFinderToDAP",
+  },
+  isAvailableInWelsh: false,
+  showInAccounts: false,
+  showInServices: false,
+  showDetailedCard: false,
+  showInActivityHistory: false,
+  showInDeleteAccount: false,
+  showInSearchableList: false,
+  translations: {
+    en: {
+      header: "Victims Pathfinder to DAP",
+      description: "",
+      linkText: "",
+      linkUrl: "",
+    },
+  },
+  isOffboarded: false,
+};
+
+export default mojVictimsPathFinderToDAP;

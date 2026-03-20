93d0cb306f029e4ef6a330adc7990638abd35124
Taher Kapasi <taher.kapasi@digital.cabinet-office.gov.uk>
2026-03-20 10:19:01 +0000

    OLH-3965: DESNZ Ofgem Financial Resilience and Welsh Gov Farming Connect

---
+++ clients/desnzOfgemFinancialResilience.ts
@@ -0,0 +1,27 @@
+import { Client } from "../interfaces/client.interface";
+
+const desnzOfgemFinancialResilience: Client = {
+  clientId: {
+    production: "fPfaL9pGNBYpxdv3vlON2BjfcII",
+    integration: "fPfaL9pGNBYpxdv3vlON2BjfcII",
+    nonProduction: "desnzOfgemFinancialResilience",
+  },
+  isAvailableInWelsh: false,
+  showInAccounts: false,
+  showInServices: false,
+  showDetailedCard: false,
+  showInActivityHistory: false,
+  showInDeleteAccount: false,
+  showInSearchableList: false,
+  translations: {
+    en: {
+      header: "Ofgem Financial Resilience",
+      description: "",
+      linkText: "",
+      linkUrl: "",
+    },
+  },
+  isOffboarded: false,
+};
+
+export default desnzOfgemFinancialResilience;

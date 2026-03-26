417243e93d302dcb7b2dbe665c560c62b26cfed0
Martin Gwarada <martin.gwarada@digital.cabinet-office.gov.uk>
2026-03-26 09:07:43 +0000

    Onboarding RPs for OfgemFinancialResilience and OfgemHeatNetworks

---
+++ clients/ofgemFinancialResilience.ts
@@ -0,0 +1,30 @@
+import { Client } from "../interfaces/client.interface";
+
+const ofgemFinancialResilience: Client = {
+  clientId: {
+    production: "fPfaL9pGNBYpxdv3vlON2BjfcII",
+    integration: "fPfaL9pGNBYpxdv3vlON2BjfcII",
+    nonProduction: "ofgemFinancialResilience",
+  },
+  isAvailableInWelsh: false,
+  showInAccounts: true,
+  showInServices: false,
+  showDetailedCard: false,
+  showInActivityHistory: true,
+  showInDeleteAccount: true,
+  showInSearchableList: true,
+
+  translations: {
+    en: {
+      header: "Ofgem Financial Resilience",
+      description:
+        "Submit regulatory information and supporting documentation to Ofgem.",
+      linkText: "Go to your Ofgem Financial Resilience account",
+      linkUrl: "regulation-portal.ofgem.gov.uk",
+      startUrl: "regulation-portal.ofgem.gov.uk",
+    },
+  },
+  isOffboarded: false,
+};
+
+export default ofgemFinancialResilience;

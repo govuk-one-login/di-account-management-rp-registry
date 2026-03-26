f11359605a6d40b0f8f266db0a6fe66f2e2b1704
Martin Gwarada <martin.gwarada@digital.cabinet-office.gov.uk>
2026-03-26 09:07:43 +0000

    Onboarding RPs for OfgemFinancialResilience and OfgemHeatNetworks

---
+++ clients/ofgemHeatNetworks.ts
@@ -0,0 +1,31 @@
+import { Client } from "../interfaces/client.interface";
+
+const ofgemHeatNetworks: Client = {
+  clientId: {
+    production: "18wwAdm_lMyUAChqxnTZWyAksFQ",
+    integration: "18wwAdm_lMyUAChqxnTZWyAksFQ",
+    nonProduction: "ofgemHeatNetworks",
+  },
+  isAvailableInWelsh: false,
+  showInAccounts: true,
+  showInServices: false,
+  showDetailedCard: false,
+  showInActivityHistory: true,
+  showInDeleteAccount: true,
+  showInSearchableList: false,
+
+  translations: {
+    en: {
+      header: "Comply with heat networks consumer protection regulations",
+      description:
+        "Start or continue providing information about your organisation and registering your heat networks with Ofgem.",
+      linkText: "Go to the Comply with heat networks consumer protection regulations service",
+      linkUrl: "https://heat-network-consumer-protections.ofgem.gov.uk",
+      startUrl: "https://www.ofgem.gov.uk/energy-regulation/low-carbon/heat-networks/comply-heat-networks-consumer-protection-regulations",
+      startText: "Ofgem Heat Networks",
+    },
+  },
+  isOffboarded: false,
+};
+
+export default ofgemHeatNetworks;

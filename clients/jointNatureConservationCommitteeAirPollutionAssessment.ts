0b47c94c1f6682bc1823579d1f4d08b5065dc774
Michael Henson <12057877+liegeandlief@users.noreply.github.com>
2026-05-19 13:32:20 +0100

    OLH-4132: update air pollution assessment link URLs

---
+++ clients/jointNatureConservationCommitteeAirPollutionAssessment.ts
@@ -0,0 +1,37 @@
+import { Client } from "../interfaces/client.interface";
+
+const jointNatureConservationCommitteeAirPollutionAssessment: Client = {
+  clientId: {
+    production: "glcH6E9VxtnCAPPwBt550zDh22Q",
+    integration: "glcH6E9VxtnCAPPwBt550zDh22Q",
+    nonProduction: "joinNatureConservationCommittee",
+  },
+  showInAccounts: true,
+  showInServices: false,
+  showDetailedCard: false,
+  showInActivityHistory: true,
+  showInDeleteAccount: true,
+  showInSearchableList: false,
+  isAvailableInWelsh: true,
+  translations: {
+    en: {
+      header: "Air pollution assessment service",
+      description:
+        "Upload environmental risk assessment results to use for in-combination assessments.",
+      linkText: "Go to your Air pollution assessment service account",
+      linkUrl: "https://archive.get-an-air-pollution-assessment.service.gov.uk",
+      startUrl: "https://www.gov.uk/guidance/get-an-air-pollution-assessment",
+    },
+    cy: {
+      header: "Gwasanaeth asesu llygredd aer",
+      description:
+        "Llwytho canlyniadau asesu risg amgylcheddol i'w defnyddio ar gyfer asesiadau cyfunol.",
+      linkText: "Ewch i'ch Cyfrif gwasanaeth asesu llygredd aer",
+      linkUrl: "https://archive.get-an-air-pollution-assessment.service.gov.uk",
+      startUrl: "https://www.gov.uk/guidance/get-an-air-pollution-assessment",
+    },
+  },
+  isOffboarded: false,
+};
+
+export default jointNatureConservationCommitteeAirPollutionAssessment;

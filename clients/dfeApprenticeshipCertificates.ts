bd13409420378c27d83bdd8f71c2b4c2af6abe85
Dana Cotoran <dana.cotoran@digital.cabinet-office.gov.uk>
2026-05-28 17:26:01 +0100

    OLH-3888: Onboard DFE Apprenticeship service

---
+++ clients/dfeApprenticeshipCertificates.ts
@@ -0,0 +1,27 @@
+import { Client } from "../interfaces/client.interface";
+
+const dfeApprenticeshipCertificates: Client = {
+  clientId: {
+    production: "CEWySFSPKpDSIsg7ooTZoaGYPLI",
+    integration: "CEWySFSPKpDSIsg7ooTZoaGYPLI",
+    nonProduction: "dfeApprenticeshipCertificates",
+  },
+  isAvailableInWelsh: false,
+  showInAccounts: true,
+  showInServices: false,
+  showInActivityHistory: true,
+  showInDeleteAccount: true,
+  showInSearchableList: false,
+  translations: {
+    en: {
+      header: "Apprenticeship certificates",
+      description:
+        "View, share or download your certificates, or view your results.",
+      linkText: "Go to your Apprenticeship certificates account",
+      linkUrl: "https://certificates.apprenticeship.education.gov.uk/certificates/list",
+    },
+  },
+  isOffboarded: false,
+};
+
+export default dfeApprenticeshipCertificates;

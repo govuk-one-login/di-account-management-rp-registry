c7326b0931cbc58254c5c5523c1378421b139acb
Martin Gwarada <martin.gwarada@digital.cabinet-office.gov.uk>
2026-06-08 15:45:25 +0100

    OLH-4190 Onboarding Early Years Teacher Financial Incentives

---
+++ clients/earlyYearsTeacherFinancialIncentives.ts
@@ -0,0 +1,29 @@
+import { Client } from "../interfaces/client.interface";
+
+const earlyYearsTeacherFinancialIncentives: Client = {
+  clientId: {
+    production: "2dwMg700-_FXDXmUK7qUzQDj_5Q",
+    integration: "2dwMg700-_FXDXmUK7qUzQDj_5Q",
+    nonProduction: "earlyYearsTeacherFinancialIncentives",
+  },
+  isAvailableInWelsh: false,
+  showInAccounts: true,
+  showInServices: false,
+  showInActivityHistory: true,
+  showInDeleteAccount: true,
+  showInSearchableList: false,
+  translations: {
+    en: {
+      header: "Claim an early years teacher recognition payment",
+      description:
+        "Start or continue an application to claim an early years teacher recognition payment.",
+      linkText: "Go to your early years teacher recognition claim",
+      linkUrl: "https://www.claim-additional-teaching-payment.service.gov.uk/early-years-teachers-financial-incentive-payments/sign-in",
+      startText: "",
+      startUrl: "https://www.claim-additional-teaching-payment.service.gov.uk/early-years-teachers-recognition-payments/landing-page",
+    },
+  },
+  isOffboarded: false,
+};
+
+export default earlyYearsTeacherFinancialIncentives;
